3220bf43a54e68d2103c2b25f0fea4245fbce151
sam-sampreeth <sampreeth.personal@gmail.com>
2024-08-22 00:14:15 +0530

    Day 23: connected email script and contact page

--- App-2/email_sender.py
+++ App-2/email_sender.py
@@ -1,17 +1,15 @@
 import smtplib, ssl
 
-host = 'smtp.gmail.com'
-port = 465
 
-username = "sam.sampreeth001@gmail.com"
-password = "mnxgxwymdfbamnzm"
+def sendEmail(message):
+    host = 'smtp.gmail.com'
+    port = 465
+    username = "sam.sampreeth001@gmail.com"
+    password = "mnxgxwymdfbamnzm"
+    receiver = "sam.sampreeth001@gmail.com"
 
-receiver = "sam.sampreeth001@gmail.com"
-message="""\
-Subject: Test Email
-Hello, how are you?
-"""
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
--- App-2/pages/Contact.py
+++ App-2/pages/Contact.py
@@ -1,11 +1,21 @@
 import streamlit as st
 from streamlit import button
 
+from email_sender import sendEmail
+
 st.header("Contact us!")
 
 with st.form(key='email_form'):
     user_email = st.text_input("Your Email Address")
     message = st.text_area("Your Message")
+    emailContent = f"""\
+Subject: Your contact form was filled by {user_email}
+
+From: {user_email}
+{message}
+"""
     submit_button = st.form_submit_button('Submit')
     if submit_button:
         print("Form was submitted")
+        sendEmail(emailContent)
+        st.info("Email sent successfully")
