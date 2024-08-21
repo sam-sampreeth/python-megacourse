261d7b0e4535b550e023e9075129ddda0a886f41
sam-sampreeth <sampreeth.personal@gmail.com>
2024-08-22 00:43:51 +0530

    Day 23: masked app password as an environment variable /day23/

--- App-2/email_sender.py
+++ App-2/email_sender.py
@@ -1,11 +1,11 @@
 import smtplib, ssl
-
+import os
 
 def sendEmail(message):
     host = 'smtp.gmail.com'
     port = 465
     username = "sam.sampreeth001@gmail.com"
-    password = "mnxgxwymdfbamnzm"
+    password = os.getenv("PASSWORD")
     receiver = "sam.sampreeth001@gmail.com"
 
     context = ssl.create_default_context()
