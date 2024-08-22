aa850cdfa1078990eac08d04e8ac48380dc6134f
sam-sampreeth <sampreeth.personal@gmail.com>
2024-08-22 23:19:37 +0530

    Day 24: Added guidelines for all pages

--- App-3/main.py
+++ App-3/main.py
@@ -10,7 +10,9 @@ for index, row in df.iterrows():
     pdf.set_font('Times', 'B', 24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1, border=0)
-    pdf.line(10, 22, 200, 22)
+
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
 
     pdf.ln(266)
     pdf.set_font('Times', 'I', 9)
@@ -23,5 +25,9 @@ for index, row in df.iterrows():
         pdf.set_font('Times', 'I', 9)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align='R', ln=1, border=0)
+
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
+
 
 pdf.output('output.pdf')
