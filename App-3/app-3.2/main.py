f12b1c1b26a9b3b330ef093927012a5073cf13d3
sam-sampreeth <sampreeth.personal@gmail.com>
2024-08-24 23:10:41 +0530

    Day 25: Started converting excel files to pdfs

--- App-3/app-3.2/main.py
+++ App-3/app-3.2/main.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, "Sheet 1")
-    print(df)
-
+    pdf = FPDF("p", "mm", "A4")
+    pdf.add_page()
+    invNum = Path(filepath).stem.split("-")[0]
+    pdf.set_font("helvetica", size=17, style="B")
+    pdf.cell(50, 8, f"Invoice #{invNum}")
+    pdf.output(f"PDFs/{invNum}.pdf")
