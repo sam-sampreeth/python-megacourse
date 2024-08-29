9db7adf48484a92e87c2fe11ca47ec22a2af4f23
sam-sampreeth <sampreeth.personal@gmail.com>
2024-08-29 22:49:29 +0530

    Day 26: Assignment

--- App-3/assignment/main.py
+++ App-3/assignment/main.py
@@ -8,5 +8,9 @@ for file in files:
     headName = Path(file).stem.title()
     pdf.add_page()
     pdf.set_font("helvetica", size=18, style="B")
-    pdf.cell(50, 8, txt=headName)
+    pdf.cell(50, 8, txt=headName, ln=1)
+    with open(file, 'r') as f:
+        content = f.read()
+    pdf.set_font("helvetica", size=14)
+    pdf.multi_cell(0, 6, txt=content)
 pdf.output("Animals.pdf")
