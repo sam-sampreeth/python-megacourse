de02d3eecc3073762f596cf5494e3c3729f61e58
sam-sampreeth <sampreeth.personal@gmail.com>
2024-09-02 19:45:49 +0530

    Day 30: Finished dictionary API exercise

--- App-6/exercise/main.py
+++ App-6/exercise/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -6,8 +7,9 @@ def home():
 
 @app.route('/api/v1/<word>')
 def api(word):
-    mean = "Sun is a star"
-    return {"word": word, "mean": mean}
+    df = pd.read_csv("dictionary.csv")
+    defn = df.loc[df['word'] == word]['definition'].squeeze()
+    return {"word": word, "mean": defn}
 
 if __name__ == '__main__':
     app.run(debug=True)
