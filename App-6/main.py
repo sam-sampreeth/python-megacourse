b2ebfcd675430f6c6d3f0abe2153c327eecba989
sam-sampreeth <sampreeth.personal@gmail.com>
2024-09-06 22:08:28 +0530

    Day 31: URL endpoint for annual data

--- App-6/main.py
+++ App-6/main.py
@@ -17,5 +17,20 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+@app.route("/api/v1/<station>")
+def allData(station):
+    fname = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(fname, skiprows=20, parse_dates=["    DATE"])
+    res = df.to_dict(orient="records")
+    return res
+
+@app.route("/api/v1/annual/<station>/<year>")
+def yearData(station, year):
+    fname = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(fname, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    res = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return res
 if __name__ == "__main__":
     app.run(debug=True)
