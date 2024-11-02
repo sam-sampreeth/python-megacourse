21281a7a7f80690ac0e6385fd300fd8d4a2d81d6
Sampreeth <sampreeth.personal@gmail.com>
2024-11-02 19:31:01 +0530

    Day 37: Worked on the mailer module which sends a relevant image of the video

--- App-9/mailer.py
+++ App-9/mailer.py
@@ -1,2 +1,32 @@
-def send_email():
-    print("Email sent")
+from email.message import EmailMessage
+
+from dotenv import load_dotenv
+import os
+import smtplib
+
+from pygments.lexers import q
+
+load_dotenv()
+app_pass = os.getenv('APP_PASS')
+app_id = os.getenv('APP_ID')
+receiver = os.getenv('APP_ID')
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New object detected"
+    email_message.set_content("I just detected a new object in the video!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype='image', subtype='PNG')
+
+    gmail = smtplib.SMTP('smtp.gmail.com', 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(app_id, app_pass)
+    gmail.sendmail(app_id, receiver, email_message.as_string())
+    gmail.quit()
+
+if __name__ == '__main__':
+    send_email(image_path="images/19.png")
--- App-9/main.py
+++ App-9/main.py
@@ -1,4 +1,5 @@
 import glob
+import os
 import cv2
 import time
 from mailer import send_email
@@ -9,6 +10,11 @@ time.sleep(1)
 frame1 = None
 status_list = []
 count = 1
+
+def cleaner():
+    images = glob.glob('images/*.png')
+    for image in images:
+        os.remove(image)
 
 while True:
     status = 0
@@ -45,7 +51,8 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_obj)
+        cleaner()
 
     print(status_list)
 
