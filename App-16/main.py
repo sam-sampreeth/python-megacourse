6f85eecce7a05d48267bb960d2123d11c0c7751e
Sampreeth <sampreeth.personal@gmail.com>
2025-01-31 00:44:19 +0530

    Day 51: Email sender function implemented.

--- App-16/main.py
+++ App-16/main.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+import os
+from dotenv import load_dotenv
+from flask_mail import Mail, Message
+
+load_dotenv()
+gmail_pass = os.getenv('APP_PASS')
+gmail_id = os.getenv('APP_USER')
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'myapp123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config["MAIL_USERNAME"] = gmail_id
+app.config["MAIL_PASSWORD"] = gmail_pass
+
 db=SQLAlchemy(app)
+
+mail=Mail(app)
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,7 +29,6 @@ class Form(db.Model):
     email_name = db.Column(db.String(80))
     date= db.Column(db.Date)
     occupation = db.Column(db.String(80))
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -29,6 +43,10 @@ def index():
         form = Form(first_name=first_name, last_name=last_name, email_name=email, date=date_obj, occupation=current_occupation)
         db.session.add(form)
         db.session.commit()
+
+        message = Message("New Submission", sender= gmail_id, recipients=[gmail_id], body = f"New Submission by {form.first_name} {form.last_name}")
+        mail.send(message)
+        flash(f"{first_name}'s form submitted!", "success")
 
     return render_template("index.html")
 
